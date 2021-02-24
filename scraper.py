d646f4203e16b0b82e206593f9434ca288437b4c
Gabriel Preite <gabriel.preite@studio.unibo.it>
2021-02-24 15:43:03 +0100

    alpha: cli, url, sec login, file size

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from getpass import getpass
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup
 import json
@@ -17,13 +18,22 @@ def init():
 
 def login():
 	#reads login info from json
-	with open("login.json") as f:
-		login = json.load(f)
-		payload = {
-			"UserName": login["user"],
-			"Password": login["password"],
-			"SAMLResponse": ""
-		}
+	login = dict()
+	try:
+		with open("login.json") as f:
+			login = json.load(f)	
+	except:#todo - add login failure handling
+		login["user"] = input("Email (gabriel.preite@studio.unibo.it): ")
+		if login["user"] == "":
+			login["user"] = "gabriel.preite@studio.unibo.it"
+		login["password"] = getpass("Password: ")
+
+	payload = {
+		"UserName": login["user"],
+		"Password": login["password"],
+		"SAMLResponse": ""
+	}
+
 	#logs in
 	p = s.post(CONFIG["url_login"], data=payload)
 
@@ -38,7 +48,7 @@ def login():
 	#sends response to complete login
 	r_payload = {"SAMLResponse": value}
 	p = s.post(CONFIG["url_saml"], data=r_payload)
-    
+
 def getlist():
 	#gets links and names from courses
 	data = []
@@ -68,17 +78,39 @@ def getlist():
 	return data
 
 def getname(link):
+	#gets course name from link
 	page = s.get(link)
 	soup = BeautifulSoup(page.content, "html.parser")
 	name = re.search("<h1>(.+)</h1>", str(soup)).group()[4:-5]
 	return name
 
+def getres(link):
+	#scans course for resources
+	page = s.get(link)
+	soup = BeautifulSoup(page.content, "html.parser")
+
+	#finds all matches for the resource link type
+	res_list = soup.find_all("a", {"class": "aalink", "href": re.compile("(.+)resource(.+)")})
+	kb = 0 #records size in kb
+	for res in res_list:
+		res_page = s.get(res.get("href"))
+		res_url = res_page.url
+		print(res_url)
+
+		#saves size in http header
+		res = s.head(res_url)
+		kb += int(res.headers["Content-Length"])/1024
+	return kb
+
 def enroll(data):
+	res_w = 0
 	for item in data:
 		try:
+			#scrape page
 			page = s.get(item["link"])
 			soup = BeautifulSoup(page.content, "html.parser")
 
+			#scrape fields from js generated code
 			r_id = soup.find("input", {"name": "id"}).get("value")
 			r_in = soup.find("input", {"name": "instance"}).get("value")
 			r_sk = soup.find("input", {"name": "sesskey"}).get("value")
@@ -86,6 +118,7 @@ def enroll(data):
 			r_qf_v = soup.find("input", {"name": r_qf_k}).get("value")
 			r_mf = soup.find("input", {"name": "mform_isexpanded_id_selfheader"}).get("value")
 
+			#create post payload
 			payload = {
 				"id": r_id,
 				"instance": r_in,
@@ -94,17 +127,27 @@ def enroll(data):
 				"mform_isexpanded_id_selfheader": r_mf
 			}
 
-			p = s.post(CONFIG["url_enroll"], data=payload)
-			print("Enrolled in: "+item["name"])
+			#send post to enroll
+			s.post(CONFIG["url_enroll"], data=payload)
+			p = s.get(item["link"])
+			soup = BeautifulSoup(p.content, "html.parser")
+			if re.search("(.+)Opzioni di iscrizione(.+)", str(soup)) == None:
+				print("Enrolled in: "+item["name"])
+			else:
+				print("Closed course: "+item["name"])
 		except:
+			#courses already enrolled in have no "enroll" button
 			print("Already enrolled in: "+item["name"])
+		res_w += getres(item["link"])
+		print(res_w)
 
 def main():
 	init()
 	login()
-	if str(sys.argv[1]) == "-u":
+	#cli argument logic
+	if len(sys.argv) > 1 and str(sys.argv[1]) == "-u":
 		data = [{"name": getname(str(sys.argv[2])),
-				"link": str(sys.argv[2])
+			"link": str(sys.argv[2])
 		}]
 	else:
 		data = getlist()
